edd77515e2b2e5d78d6fafdf8b9804b99fcf6e23
unknown <bolotmartin@gmail.com>
2016-01-06 22:30:34 +0100

    reorganisation pour ui

--- app.js
+++ app.js
@@ -8,9 +8,18 @@ requirejs.config({
 });
 // calling main function
 requirejs(
-    ["../main"
+    [
+    "../main"
     //lodash (mostly for object cloning)
     ,"https://cdn.jsdelivr.net/lodash/3.10.1/lodash.js"
     //sweetalert (for alerts and prompts)
     ,"res/js/sweetalert.min.js"
+    //jQuery
+    ,"http://code.jquery.com/jquery-2.1.4.min.js"
+    //jQuery UI
+    ,"https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"
+    //QUnit
+    ,"http://code.jquery.com/qunit/qunit-1.20.0.js"
+    //d3
+    ,"http://d3js.org/d3.v3.min.js"
 ]);
