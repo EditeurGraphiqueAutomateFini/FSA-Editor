d0ee0905e30c92abe0cee216b6aeadd6e800b6bd
unknown <bolotmartin@gmail.com>
2016-01-07 15:01:44 +0100

    correctin require jquery

--- app.js
+++ app.js
@@ -3,23 +3,27 @@ requirejs.config({
     baseUrl: "app/src/",    //all scripts will be called from this path
     paths: {
         data: "../../data", //except for local datas (for local testing)
-        test: "../test/"    //and tests
+        test: "../test/",    //and tests
+        jquery : "http://code.jquery.com/jquery-2.1.4.min"  //defining jquery as a module
     }
 });
 // calling main function
 requirejs(
     [
-    "../main"
-    //lodash (mostly for object cloning)
-    ,"https://cdn.jsdelivr.net/lodash/3.10.1/lodash.js"
-    //sweetalert (for alerts and prompts)
-    ,"res/js/sweetalert.min.js"
-    //jQuery
-    ,"http://code.jquery.com/jquery-2.1.4.min.js"
-    //jQuery UI
-    ,"https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"
-    //QUnit
-    ,"http://code.jquery.com/qunit/qunit-1.20.0.js"
-    //d3
-    ,"http://d3js.org/d3.v3.min.js"
+        //libs first
+        //jQuery
+        "jquery"
+        //lodash (mostly for object cloning)
+        ,"https://cdn.jsdelivr.net/lodash/3.10.1/lodash.js"
+        //sweetalert (for alerts and prompts)
+        ,"res/js/sweetalert.min.js"
+        //jQuery UI
+        ,"https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"
+        //QUnit
+        ,"http://code.jquery.com/qunit/qunit-1.20.0.js"
+        //d3
+        ,"http://d3js.org/d3.v3.min.js"
+
+        //main
+        ,"../main"
 ]);
