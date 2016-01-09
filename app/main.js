99b072066c2751ea52b2775d3f65d677c193bfec
unknown <bolotmartin@gmail.com>
2016-01-09 13:35:18 +0100

    modifs menu fonctionnel

--- app/main.js
+++ app/main.js
@@ -1,20 +1,4 @@
 // main function
 define(function(require){
-    var menu = require("menu/menu"),
-        server = require("utility/server_request");
-
-    //checking menu function's return value
-    switch (menu) {
-        case "view":
-            //initiate view mode w/ array of states
-            server.getRequest("view");
-            break;
-        case "edit":
-            server.getRequest("edit");
-            break;
-        case "create":
-            break;
-        default:
-            server.getRequest("view");
-    }
+    var menu = require("menu/menu");
 });
--- app/src/menu/menu.js
+++ app/src/menu/menu.js
@@ -1,30 +1,34 @@
-define(function(){
+define(function(require){
+    var server = require("utility/server_request")
     //return menu value (view : viewmode, edit: editmode)
     if(location.href.indexOf("test")!=-1){  //pour éviter d'avoir le menu en test, ajouter "?test=1" au bout de son url
         //exemple file:///D:/Martin/_Cours/IUT/Projet%20tuteur%C3%A9/Editeur_graphique_projet/index.html?test=1
-        return "edit";
+            server.getRequest("edit"); // Editor mode
     }else{
         //code ici pour définir le menu
         $(document).ready(function(){
-            $("body").prepend("<div class='menu'></div>")
-                .html("<ul id=\"menu\"><li id=\"view\"><a href=\"#\">Mode vue</a></li><li id=\"editor\"><a href=\"#\">Mode edition</a></li></ul>");
-
+            $("body").prepend("<div class='menu'></div>");
+            $(".menu").html("<ul id=\"menu\"><li id=\"view\"><a href=\"#\">Mode vue</a></li><li id=\"edit\"><a href=\"#\">Mode edition</a></li></ul>");
 
     		var li = document.getElementById("menu").getElementsByTagName("li");
     		for(var i=0;i<li.length;i++){
         		var liItem = document.getElementById(li[i].id);
         		liItem.addEventListener("click",function(e){
-            		e.preventDefault(); // block the event
-            		if( this.id == "view" ){
-        				alert("mode vue"); // View mode
-            		}
-            		else if ( this.id == "editor"){
-        				alert("mode editor"); // Editor mode
-            		}
-            		else{
-        				alert("error");  // error if there's a problem
-
-            		}
+                    var id = this.id;
+            		e.preventDefault(); // blocks the click event
+                    switch (id) {   //checking clicked element's id value
+                        case "view":
+                            server.getRequest("view");  // View mode
+                            break;
+                        case "edit":
+    		                server.getRequest("edit"); // Editor mode
+                            break;
+                        case "create":
+                            break;
+                        default:
+                            server.getRequest("view");
+                    }
+                    $(".menu").hide();
     	       });
     	    }
         });
