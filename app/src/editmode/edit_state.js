19fb1d01acf382f48a31877cc9381e406049e8ac
Martin Bolot <bolotmartin@gmail.com>
2016-01-31 15:30:09 +0100

    correction bug maxnoise

--- app/src/editmode/edit_state.js
+++ app/src/editmode/edit_state.js
@@ -1,12 +1,15 @@
 define(function(){
     //edit selected state's properties
     return function(d,inputValue,context){
+      console.log(d);
         var oldName = d.name;
         d.name = inputValue;
         context.getData.states[d.name] = context.getData.states[oldName];
         context.getData.states[oldName] = undefined;
-        d3.select("text#state_name_"+d.index).html(function(d){return d.name;});
-        console.log(context.getData);
+        d3.select("text#state_name_"+d.index).html(function(d){
+          var nameText = d.max_noise>0 ? d.name+"["+d.max_noise+"]" : d.name;
+          return nameText;
+        });
         //restart force layout w/ new data
         context.force.start();
     }
