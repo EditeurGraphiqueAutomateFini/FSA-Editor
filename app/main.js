58150232dfe213fbd7f0ab7b25c0456f2d59dfe9
unknown <bolotmartin@gmail.com>
2016-02-12 23:18:53 +0100

    modifs panneau + suppression objet FE bottom

--- app/main.js
+++ app/main.js
@@ -2,21 +2,19 @@
 define(function(require){
     var menu = require("menu/menu");
 
-    $("#object_textarea label").click(function(){
-        var forAttr = $(this).attr("for");
-        $("#"+forAttr).select();
-    });
-    $("#object_container_close").click(function(){
+    closeContainer();
+    $("#object_container_close").click(closeContainer);
+    function closeContainer(){
         $("#object_container_wrapper").toggleClass("closed");
+        $(this).toggleClass("panel_closed");
         if($(this).hasClass("panel_closed")){
             $("#object_container_close")
-                .html("X")
+                .html(">")
                 .prependTo("#object_container_wrapper");
         }else{
             $("#object_container_close")
                 .html("<")
                 .prependTo("body");
         }
-        $(this).toggleClass("panel_closed");
-    })
+    }
 });
