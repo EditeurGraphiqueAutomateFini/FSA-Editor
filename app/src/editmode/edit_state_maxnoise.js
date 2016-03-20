26362f2ba2856843e2679a3fa9328183b05a794a
Martin Bolot <bolotmartin@gmail.com>
2016-03-20 12:19:51 +0100

    bugfix maxnoise edition

--- app/src/editmode/edit_state_maxnoise.js
+++ app/src/editmode/edit_state_maxnoise.js
@@ -5,7 +5,7 @@ define(function(){
         d.max_noise = inputValue;
         context.getData.states[d.name].max_noise = inputValue;
         d3.select("text#state_name_"+d.index+" tspan.state_name_maxnoise").html(function(d){
-          return "["+d.max_noise+"]";
+          return d.max_noise>0 ? "["+d.max_noise+"]" : "";
         });
 
         //restart force layout w/ new data
