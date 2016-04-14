940d89ef97e191b8b53b4f8bfb73a61958d59a2c
Martin Bolot <bolotmartin@gmail.com>
2016-04-14 13:45:50 +0200

    added modules for each editable state property

--- app/src/editmode/edit_state.js
+++ app/src/editmode/edit_state.js
@@ -2,11 +2,12 @@ define(function(require){
     return function(newValues,d,context){   //newValues includes newName,newTerminal,newMaxNoise,newMaxTotalNoise,newMaxDuration,newMaxTotalDuration,newDefaultTransition
         var edit_default_transition = require("editmode/edit_default_transition"),
             edit_references = require("editmode/edit_references"),
+            edit_state_maxduration = require("editmode/edit_state_maxduration"),
+            edit_state_maxtotalduration = require("editmode/edit_state_maxtotalduration"),
             edit_state_maxnoise = require("editmode/edit_state_maxnoise"),
-            edit_state_name = require("editmode/edit_state_name");
-
-
-        console.log(_.clone(newValues.newDefaultTransition),d);
+            edit_state_maxtotalnoise = require("editmode/edit_state_maxtotalnoise"),
+            edit_state_name = require("editmode/edit_state_name"),
+            edit_state_terminal = require("editmode/edit_state_terminal");
 
         //edit default_transition if necessary
         if(newValues.newDefaultTransition){
@@ -23,7 +24,7 @@ define(function(require){
         }
         //edit max_duration if necessary
         if(newValues.newMaxDuration !== d.max_duration){
-
+            edit_state_maxduration(d,newValues.newMaxDuration,context);
         }
 
         //edit max_noise if necessary
@@ -33,12 +34,12 @@ define(function(require){
 
         //edit max_total_duration if necessary
         if(newValues.newMaxTotalDuration !== d.max_total_duration){
-
+            edit_state_maxtotalduration(d,newValues.newMaxTotalDuration,context);
         }
 
         //edit max_total_noise if necessary
         if(newValues.newMaxTotalNoise !== d.max_total_noise){
-
+            edit_state_maxtotalnoise(d,newValues.newMaxTotalNoise,context);
         }
 
         //edit name if necessary
@@ -49,9 +50,10 @@ define(function(require){
 
         //edit terminal if necessary
         if(newValues.newTerminal !== d.terminal){
-
+            edit_state_terminal(d,newValues.newTerminal,context);
         }
 
-        //reload ?
+        //restart force layout w/ new data
+        context.force.start();
     }
 });
---
+++ app/src/editmode/edit_state_maxtotalduration.js
@@ -0,0 +1,8 @@
+define(function(){
+    return function(d,newMaxTotalDuration,context){
+        d.max_total_duration = newMaxTotalDuration;
+        if(context.getData.states[d.name]){
+            context.getData.states[d.name].max_total_duration = newMaxTotalDuration;
+        }
+    }
+});
