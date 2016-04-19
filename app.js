924d9377643d0548526d786aa0844e99b29f3f7c
Martin Bolot <bolotmartin@gmail.com>
2016-04-19 20:25:31 +0200

    added fsa_editor main function

--- app.js
+++ app.js
@@ -2,31 +2,28 @@
 require.config({
     baseUrl: "app/src/",    // all scripts will be called from this path
     paths: {
-        data: "../../data" // except for local datas (for local testing)
-        ,test: "../test/"    // and tests
-        ,jquery : "http://code.jquery.com/jquery-2.1.4.min"  // defining jquery as a module
-        ,swal: "../../res/js/sweetalert.min"
+        "data": "../../data" // except for local datas (for local testing)
+        ,"test": "../test/"    // and tests
+        ,"jquery": "http://code.jquery.com/jquery-2.1.4.min"  // defining jquery as a module
+        ,"lodash": "https://cdn.jsdelivr.net/lodash/3.10.1/lodash.js" // lodash (mostly for object cloning)
+        //,"jqueryui": "https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js" // jQuery UI
+        ,"qunit": "http://code.jquery.com/qunit/qunit-1.20.0.js" // QUnit
+        ,"d3": "http://d3js.org/d3.v3.min.js" // d3
+        ,"swal": "../../res/js/sweetalert.min" // sweetalert (for alerts and prompts)
     }
 });
 // calling main function
 require(
     [
         // libs first
-        // jQuery
-        //"jquery"
-        "http://code.jquery.com/jquery-2.1.4.min.js"
-        // lodash (mostly for object cloning)
+        "jquery"
         ,"https://cdn.jsdelivr.net/lodash/3.10.1/lodash.js"
-        // sweetalert (for alerts and prompts)
-        ,"swal"
-        // jQuery UI
-        ,"https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"
-        // QUnit
         ,"http://code.jquery.com/qunit/qunit-1.20.0.js"
-        // d3
         ,"http://d3js.org/d3.v3.min.js"
-
+        ,"swal"
         // main
         ,"../main"
-    ]
+    ],function(jquery,_,qunit,d3,swal,fsa_editor){
+        fsa_editor();
+    }
 );
--- app/main.js
+++ app/main.js
@@ -1,30 +1,35 @@
 // main function
 define(function(require){
-    require("menu/menu");
+    return window.fsa_editor = function(data,mode,options){
+        console.log(data,mode,options);
 
-    //right panel
-    closeContainer("#object_container_wrapper","#object_container_close","left");
-    $("#object_container_close").click(function(){
+        var menu = require("menu/menu");
+        menu();
+
+        //right panel
         closeContainer("#object_container_wrapper","#object_container_close","left");
-    });
+        $("#object_container_close").click(function(){
+            closeContainer("#object_container_wrapper","#object_container_close","left");
+        });
 
-    //left pannel
-    closeContainer("#object_container_wrapper_left","#object_container_close_right","right");
-    $("#object_container_close_right").click(function(){
+        //left pannel
         closeContainer("#object_container_wrapper_left","#object_container_close_right","right");
-    });
+        $("#object_container_close_right").click(function(){
+            closeContainer("#object_container_wrapper_left","#object_container_close_right","right");
+        });
 
-    function closeContainer(wrapper,button,orientation){
-        $(wrapper).toggleClass("closed");
-        if($(button).hasClass("panel_closed")){
-            $(button)
-                .html((orientation === "left" ? ">" : "<"))
-                .prependTo(wrapper);
-        }else{
-            $(button)
-                .html((orientation === "left" ? "<" : ">"))
-                .prependTo("body");
+        function closeContainer(wrapper,button,orientation){
+            $(wrapper).toggleClass("closed");
+            if($(button).hasClass("panel_closed")){
+                $(button)
+                    .html((orientation === "left" ? ">" : "<"))
+                    .prependTo(wrapper);
+            }else{
+                $(button)
+                    .html((orientation === "left" ? "<" : ">"))
+                    .prependTo("body");
+            }
+            $(button).toggleClass("panel_closed");
         }
-        $(button).toggleClass("panel_closed");
-    }
+    };
 });
