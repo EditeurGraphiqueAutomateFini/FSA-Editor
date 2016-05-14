5bb5af02eedc3ebc24201cde2455d5f6259876be
Martin Bolot <bolotmartin@gmail.com>
2016-05-14 15:39:18 +0200

    added encapsulated file with less

--- app.js
+++ app.js
@@ -3,11 +3,9 @@
 *   those will be added as a tag in "head" by webpack
 */
 // requiring main stylesheet
-require("./styles.css");
+require("./styles.less");
 // requiring sweetalert stylesheet
 require("./res/css/sweetalert.css");
-// requiring test stylesheet - must be removed in production
-require("./res/css/tests.css");
 
 // requiring app
 var app = require("./app/main.js");
