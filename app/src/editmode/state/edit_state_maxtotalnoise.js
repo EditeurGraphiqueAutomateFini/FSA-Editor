23e105f89c532ae0c1441e03e47a9072e3bd301d
Martin Bolot <bolotmartin@gmail.com>
2016-05-21 16:14:30 +0200

    edited default values for state attributes

--- app/src/editmode/state/edit_state_maxtotalnoise.js
+++ app/src/editmode/state/edit_state_maxtotalnoise.js
@@ -10,7 +10,7 @@ define(function(){
     *   @alias module:editmode/state/edit_state_maxtotalnoise
     */
     var edit_state_maxtotalnoise = function(d,newMaxTotalNoise,context){
-        if(newMaxTotalNoise){
+        if(newMaxTotalNoise >= 0){
             d.max_total_noise = newMaxTotalNoise;
             if(context.getData.states[d.name]){
                 context.getData.states[d.name].max_total_noise = parseInt(newMaxTotalNoise) || 0;
